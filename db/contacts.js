e542611103e879b617a4bb37752cda56423f52fe
Leonid578 <76591317+Leonid578@users.noreply.github.com>
2022-08-10 13:12:20 +0300

    Update contacts.js

--- db/contacts.js
+++ db/contacts.js
@@ -10,37 +10,32 @@ async function listContacts() {
 }
 
 async function getContactById(contactId) {
-  const data = await fs.readFile(contactPath);
-  const allContacts = JSON.parse(data);
-  const contactByID = await allContacts.find(elem => elem.id === contactId);
+  const data = await listContacts();
+  const contactByID = allContacts.find(elem => elem.id === contactId);
   return contactByID ? contactByID : null;
 }
 
 async function removeContact(contactId) {
-  const data = await fs.readFile(contactPath);
-  const allContacts = JSON.parse(data);
+  const data = await listContacts();
   const contactByID = allContacts.find(elem => elem.id === contactId);
   if (!contactByID) return null;
-  const spliseContacts = allContacts.filter(el => el.id !== contactId);
-  await fs.writeFile(contactPath, JSON.stringify(spliseContacts));
+  const removedContact = allContacts.filter(el => el.id !== contactId);
+  await fs.writeFile(contactPath, JSON.stringify(removedContact));
   return contactByID;
 }
 
 async function addContact(name, email, phone) {
   const newContact = { id: nanoid(), name, email, phone };
-  const data = await fs.readFile(contactPath);
-  const allContacts = JSON.parse(data);
+  const data = await listContacts();
   allContacts.push(newContact);
   await fs.writeFile(contactPath, JSON.stringify(allContacts));
   return newContact;
 }
 
 
-const contactsOperation = {
+module.exportsn = {
   listContacts,
   getContactById,
   removeContact,
   addContact,
 };
-
-module.exports = contactsOperation;
